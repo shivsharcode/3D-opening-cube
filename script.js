288905f72af6626cddb5f34eafdd9740206e1788
shivsharcode <shivshar16@outlook.com>
2024-06-30 17:05:42 +0530

    Project completed

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+// const btn = document.getElementById('toggleButton') ;
+
+// btn.addEventListener('click', function(){
+//     let toggleBox = document.get('toggleBox') ;
+//     toggleBox.classList.toggle('active') ;
+// })
+
+document.getElementById('toggleButton').addEventListener('click', function() {
+    var toggleBox = document.getElementById('toggleBox');
+    toggleBox.classList.toggle('active');
+
+    
+});
